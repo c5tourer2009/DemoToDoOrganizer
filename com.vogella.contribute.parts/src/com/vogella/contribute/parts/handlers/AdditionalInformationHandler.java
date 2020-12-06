ca6555de84fd1653ad5319a5187d06abcc0aca31
Dzieyk, Alexander GUEP <alexander.dzieyk@guep.com>
2020-12-06 23:45:37 +0100

    renamed plugin command

--- com.vogella.contribute.parts/src/com/vogella/contribute/parts/handlers/AdditionalInformationHandler.java
+++ com.vogella.contribute.parts/src/com/vogella/contribute/parts/handlers/AdditionalInformationHandler.java
@@ -1,10 +1,15 @@
 package com.vogella.contribute.parts.handlers;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 
 public class AdditionalInformationHandler {
 	@Execute
-    public void execute() {
-      System.out.println((this.getClass().getSimpleName() + " called"));
+    public void execute(Shell shell) {
+		
+		MessageDialog.openInformation(shell, 
+				"Plugin Command", 
+				"I am a plugin command");
     }
 }
