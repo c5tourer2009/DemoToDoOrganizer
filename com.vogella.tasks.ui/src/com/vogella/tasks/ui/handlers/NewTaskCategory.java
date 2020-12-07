9f8a907557f87166b3013222f914bc913d4a833f
Dzieyk, Alexander GUEP <alexander.dzieyk@guep.com>
2020-12-07 03:45:37 +0100

    add new edit category handler

--- com.vogella.tasks.ui/src/com/vogella/tasks/ui/handlers/NewTaskCategory.java
+++ com.vogella.tasks.ui/src/com/vogella/tasks/ui/handlers/NewTaskCategory.java
@@ -1,5 +1,13 @@
 package com.vogella.tasks.ui.handlers;
 
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.workbench.IWorkbench;
+import org.eclipse.swt.widgets.Shell;
+
 public class NewTaskCategory {
 
+	@Execute
+    public void execute(IWorkbench workbench, Shell shell) {
+		//ToDo
+    }
 }
--- com.vogella.tasks.ui/src/com/vogella/tasks/ui/handlers/RemoveTaskCategory.java
+++ com.vogella.tasks.ui/src/com/vogella/tasks/ui/handlers/RemoveTaskCategory.java
@@ -1,5 +1,13 @@
 package com.vogella.tasks.ui.handlers;
 
-public class RemoveTaskCategory {
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.workbench.IWorkbench;
+import org.eclipse.swt.widgets.Shell;
 
+public class RemoveTaskCategory {
+	
+	@Execute
+    public void execute(IWorkbench workbench, Shell shell) {
+		//ToDo
+    }
 }
